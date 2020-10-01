d22f09f4773d4407ae275c6b7618796821069f22
hemsleek <asiyanbimubashir@gmail.com>
2020-10-01 13:06:26 +0100

    feat: phonebook name add

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react'
 
-
-import './App.scss';
+import './App.scss'
 
 const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' },
+    {name:'Ada lovelace'}
+  ]) 
+  const [ newName, setNewName ] = useState('')
 
   return (
     <div className="App">
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+        {
+
+        persons.map(person => (<p key={person.name}>{person.name}</p>))
+        
+        }
       
     </div>
-  );
+  )
 }
-
-export default App;
+export default App
