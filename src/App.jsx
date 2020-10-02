7a40d3fa5a4cb91e7e95ac54c6d0eb39e4e4b7a1
hemsleek <asiyanbimubashir@gmail.com>
2020-10-02 17:51:21 +0100

    feat: phonebook search

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,51 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 
 import './App.scss'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
     { name: 'Arto Hellas', number: '08108170354' },
-    {name:'Ada lovelace' , number:'07036156182'}
+    {name:'Ada Lovelace' , number:'07036156182'},
+    { name: 'Zulema Zahil', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
+  const [filtered, setFiltered] = useState('')
  
+  const personsToDisplay= filtered.trim()? 
+                        persons.filter(person => person.name.toLowerCase().indexOf(filtered.trim().toLowerCase()) > -1) : persons
+  useEffect(() => {
+    //localstorage persist
+  }, [])
 
   const handleNewPerson = (e) =>{
       e.preventDefault()
-      const name = e.target.name.value
-      const number = e.target.number.value
+      const form = e.target
+      const name =form.name.value
+      const number = form.number.value
 
       const newContact = {name, number}
-     
-
-      // setNewName(newContact)
+    
 
       if(persons.some(person=> person.name.toLowerCase() === name.trim().toLowerCase())){
         window.alert(`${name} is already added to phonebook`)
 
       }else{
         setPersons(persons.concat(newContact))
-  }
+        form.reset()
+        }
+
 }
  
   return (
     <div className="App">
       <h2>Phonebook</h2>
+      
+      <input 
+        type="search" value={filtered} placeholder="Search"
+        onChange={e => setFiltered(e.target.value)}
+      />
+
       <form onSubmit={handleNewPerson}>
         <div className='form-group'>
           <label htmlFor="name">Name</label>
@@ -45,7 +61,7 @@ const App = () => {
           <input
             id='number' type="number" 
             placeholder="Add Number" 
-            name="number"
+            name="number" required
           />
         </div>
           <div>
@@ -59,7 +75,7 @@ const App = () => {
 
           {
           
-          persons.map(person => (<li key={person.name}>{person.name} - {person.number}</li>))
+          personsToDisplay.map(person => (<li key={person.name}>{person.name} - {person.number}</li>))
           
           }
       
